e45e431b923a6b63147dc7295656024669fb8d32
Salymbek <calumbekcaatbekov@gmail.com>
2022-12-15 13:27:42 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.*;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        Integer[] integers1 = new Integer[]{2,4,3,1,6,5,9,8,7,10,13,11,12,15,14};
+
+        List<Integer>integers = new ArrayList<>();
+
+
+
+        for (int i = 0; i < integers1.length ; i++) {
+            if (integers1[i] % 2 == 1){
+                integers.add(integers1[i]);
+            }
+        }
+
+
+        for (Integer in2 : integers) {
+            System.out.print("["+in2+"]   -   ");
+            Stream.of(in2).forEach(x -> System.out.println(x * x));
+        }
+
+
+
+       Optional<Integer> max = integers.stream().max(Comparator.comparing(integer -> integer));
+        System.out.println(max);
+
+
     }
 }
